1dd1aef37ea5bd7caa9ffd4da9167ef26af75a65
yeonui-0626 <73818604+yeonui-0626@users.noreply.github.com>
2022-05-25 21:34:26 +0900

    user interest apt

--- happyhouse_vue/src/components/Index/IndexHouseArround.vue
+++ happyhouse_vue/src/components/Index/IndexHouseArround.vue
@@ -5,10 +5,11 @@
         <div class="md-layout">
           <div class="md-layout-item">
             <div class="brand">
-              <h1 style="margin-top:0.5em">House Arround</h1>
+              <h1 style="margin-top:0.5em">Search by Marker</h1>
               <div style="text-align:center; margin-top: 8em">
                 <h4>
-                  설명설명설명설명!!!!!
+                  선호하는 위치를 마커로 표시하고 중심지 주변 아파트를 조회할 수
+                  있습니다.
                 </h4>
                 <router-link to="/aroundapt">
                   <b-button variant="primary" style="margin-top:1em"
